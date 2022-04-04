2e409b80557e21f1ac6c69c3259eae75fcc37718
Максим Корман <mkorman359@gmail.com>
2022-04-04 18:09:58 +0300

    Add project files.

---
+++ Lab_3_NM/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace Lab_3_NM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button_calculate = new System.Windows.Forms.Button();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox_error = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_clear
+            // 
+            this.button_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_clear.Location = new System.Drawing.Point(183, 295);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(66, 66);
+            this.button_clear.TabIndex = 10;
+            this.button_clear.Text = "AC";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // button_calculate
+            // 
+            this.button_calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_calculate.Location = new System.Drawing.Point(27, 295);
+            this.button_calculate.Name = "button_calculate";
+            this.button_calculate.Size = new System.Drawing.Size(150, 30);
+            this.button_calculate.TabIndex = 9;
+            this.button_calculate.Text = "Обчислити";
+            this.button_calculate.UseVisualStyleBackColor = true;
+            this.button_calculate.Click += new System.EventHandler(this.button_calculate_Click);
+            // 
+            // button_exit
+            // 
+            this.button_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_exit.Location = new System.Drawing.Point(27, 331);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(150, 30);
+            this.button_exit.TabIndex = 8;
+            this.button_exit.Text = "Вихід";
+            this.button_exit.UseVisualStyleBackColor = true;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click_1);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(23, 272);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 20);
+            this.label10.TabIndex = 50;
+            this.label10.Text = "e =";
+            // 
+            // textBox_error
+            // 
+            this.textBox_error.Location = new System.Drawing.Point(62, 272);
+            this.textBox_error.Name = "textBox_error";
+            this.textBox_error.Size = new System.Drawing.Size(69, 20);
+            this.textBox_error.TabIndex = 49;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(404, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "label1";
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(62, 169);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(100, 20);
+            this.textBox_a.TabIndex = 52;
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Location = new System.Drawing.Point(62, 191);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(100, 20);
+            this.textBox_b.TabIndex = 53;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 20);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "a =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(25, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 55;
+            this.label3.Text = "b =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(168, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "Знак -";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(168, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 20);
+            this.label5.TabIndex = 57;
+            this.label5.Text = "Знак +";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(404, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 58;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(404, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 59;
+            this.label7.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_b);
+            this.Controls.Add(this.textBox_a);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox_error);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_calculate);
+            this.Controls.Add(this.button_exit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Button button_calculate;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox_error;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Lab_3_NM/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_3_NM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_exit_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button_calculate_Click(object sender, EventArgs e)
+        {
+            double x_s, a_s, a, a_h, b, b_h, b_s, a_ss, b_ss, error;
+
+            double[] x = new double[4];
+
+            error = Convert.ToDouble(textBox_error.Text);
+            a = Convert.ToDouble(textBox_a.Text);
+            b = Convert.ToDouble(textBox_b.Text);
+
+            a_h = a;
+            b_h = b;
+            
+            for (int i = 0; i < 1000; i++)
+            {
+                x[0] = ((a_h + b_h) / 2);
+
+                a_s = Math.Pow(a_h - 1, 2) - (1.5 * Math.Exp(a_h));
+
+                x_s = Math.Pow(x[0] - 1, 2) - (1.5 * Math.Exp(x[0]));
+
+                if (Math.Sign(x_s) == Math.Sign(a_s))
+                {
+                    a_h = x[0];
+                }
+                if (Math.Sign(x_s) != Math.Sign(a_s))
+                {
+                    b_h = x[0];
+                }
+                if (Math.Abs((b_h - a_h) / Math.Pow(2, i + 1)) <= error)
+                {
+                    break;
+                }
+            }
+            label1.Text = Convert.ToString(x[0]);
+
+            a_h = a;
+            b_h = b;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                x_s = x[1];
+                x[1] = (2 * Math.Pow(x[1], 2) - 3 * x[1] * Math.Exp(x[1]) - 2 + 3 * Math.Exp(x[1])) / (4 * x[1] - 4 - 3 * Math.Exp(x[1]));
+                a_s = Math.Pow(a_h - 1, 2) - (1.5 * Math.Exp(a_h));
+                b_s = Math.Pow(b_h - 1, 2) - (1.5 * Math.Exp(b_h));
+
+                a_ss = 2 - (3 * Math.Exp(a_h) / 2);
+                b_ss = 2 - (3 * Math.Exp(b_h) / 2);
+                
+                if (Math.Abs(x[1] - x_s) <= error)
+                {
+                    break;
+                }
+
+                if (a_s * a_ss > 0)
+                {
+                    a_h = x[1];
+                }
+
+                if (b_s * b_ss > 0)
+                {
+                    b_h = x[1];
+                }
+                
+            }
+
+            label6.Text = Convert.ToString(x[1]);
+
+            a_h = a;
+            b_h = b;
+
+            for (int i = 0; i < 1000; i++)
+            {
+              
+                b_h = (2 * Math.Pow(b_h, 2) - 3 * b_h * Math.Exp(b_h) - 2 + 3 * Math.Exp(b_h)) / (4 * b_h - 4 - 3 * Math.Exp(b_h));
+
+                a_h = (2 * a_h * b_h - 2 + 3 * Math.Exp(x[2])) / (2 * b_h - 4 + 2 * a_h);
+
+                
+
+                if (Math.Abs(b_h - a_h) <= error)
+                {
+                    break;
+                }  
+
+                x[2] = a_h + b_h / 2;
+            }
+
+            label7.Text = Convert.ToString(x[2]);
+
+        }
+
+        public double half_dec()
+        {
+            
+            return 0;
+        }
+    }
+}
