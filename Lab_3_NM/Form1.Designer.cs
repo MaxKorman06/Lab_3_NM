86fbfc108850fd36836fe38183b2492fa0bb0180
Максим Корман <mkorman359@gmail.com>
2022-04-04 20:54:55 +0300

    Non systems of equations

--- Lab_3_NM/Form1.Designer.cs
+++ Lab_3_NM/Form1.Designer.cs
@@ -34,15 +34,26 @@ namespace Lab_3_NM
             this.button_exit = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
             this.textBox_error = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.textBox_a = new System.Windows.Forms.TextBox();
             this.textBox_b = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label_half = new System.Windows.Forms.Label();
+            this.label_halfi = new System.Windows.Forms.Label();
+            this.label_tangenti = new System.Windows.Forms.Label();
+            this.label_tangent = new System.Windows.Forms.Label();
+            this.label_chordi = new System.Windows.Forms.Label();
+            this.label_chord = new System.Windows.Forms.Label();
+            this.label_combi = new System.Windows.Forms.Label();
+            this.label_comb = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button_clear
@@ -95,25 +106,16 @@ namespace Lab_3_NM
             this.textBox_error.Size = new System.Drawing.Size(69, 20);
             this.textBox_error.TabIndex = 49;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(404, 198);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 51;
-            this.label1.Text = "label1";
-            // 
             // textBox_a
             // 
-            this.textBox_a.Location = new System.Drawing.Point(62, 169);
+            this.textBox_a.Location = new System.Drawing.Point(60, 169);
             this.textBox_a.Name = "textBox_a";
             this.textBox_a.Size = new System.Drawing.Size(100, 20);
             this.textBox_a.TabIndex = 52;
             // 
             // textBox_b
             // 
-            this.textBox_b.Location = new System.Drawing.Point(62, 191);
+            this.textBox_b.Location = new System.Drawing.Point(60, 191);
             this.textBox_b.Name = "textBox_b";
             this.textBox_b.Size = new System.Drawing.Size(100, 20);
             this.textBox_b.TabIndex = 53;
@@ -122,7 +124,7 @@ namespace Lab_3_NM
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(25, 167);
+            this.label2.Location = new System.Drawing.Point(23, 167);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(31, 20);
             this.label2.TabIndex = 54;
@@ -132,7 +134,7 @@ namespace Lab_3_NM
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(25, 191);
+            this.label3.Location = new System.Drawing.Point(23, 191);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(31, 20);
             this.label3.TabIndex = 55;
@@ -142,7 +144,7 @@ namespace Lab_3_NM
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(168, 167);
+            this.label4.Location = new System.Drawing.Point(166, 167);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 20);
             this.label4.TabIndex = 56;
@@ -152,44 +154,177 @@ namespace Lab_3_NM
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(168, 191);
+            this.label5.Location = new System.Drawing.Point(166, 191);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(59, 20);
             this.label5.TabIndex = 57;
             this.label5.Text = "Знак +";
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(255, 38);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(300, 20);
+            this.label9.TabIndex = 61;
+            this.label9.Text = "Метод дихоромії (половинного поділу)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(255, 138);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(219, 20);
+            this.label11.TabIndex = 62;
+            this.label11.Text = "Метод дотичних (Ньютона)";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(255, 240);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(271, 20);
+            this.label12.TabIndex = 63;
+            this.label12.Text = "Метод хорд (пропорційних частин)";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(255, 341);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(305, 20);
+            this.label13.TabIndex = 64;
+            this.label13.Text = "Комбінований метод дотичних та хорд";
+            // 
+            // label_half
+            // 
+            this.label_half.AutoSize = true;
+            this.label_half.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_half.Location = new System.Drawing.Point(255, 58);
+            this.label_half.Name = "label_half";
+            this.label_half.Size = new System.Drawing.Size(29, 20);
+            this.label_half.TabIndex = 65;
+            this.label_half.Text = "X=";
+            // 
+            // label_halfi
+            // 
+            this.label_halfi.AutoSize = true;
+            this.label_halfi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_halfi.Location = new System.Drawing.Point(255, 78);
+            this.label_halfi.Name = "label_halfi";
+            this.label_halfi.Size = new System.Drawing.Size(29, 20);
+            this.label_halfi.TabIndex = 66;
+            this.label_halfi.Text = "N=";
+            // 
+            // label_tangenti
+            // 
+            this.label_tangenti.AutoSize = true;
+            this.label_tangenti.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_tangenti.Location = new System.Drawing.Point(255, 180);
+            this.label_tangenti.Name = "label_tangenti";
+            this.label_tangenti.Size = new System.Drawing.Size(29, 20);
+            this.label_tangenti.TabIndex = 68;
+            this.label_tangenti.Text = "N=";
+            // 
+            // label_tangent
+            // 
+            this.label_tangent.AutoSize = true;
+            this.label_tangent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_tangent.Location = new System.Drawing.Point(255, 160);
+            this.label_tangent.Name = "label_tangent";
+            this.label_tangent.Size = new System.Drawing.Size(29, 20);
+            this.label_tangent.TabIndex = 67;
+            this.label_tangent.Text = "X=";
+            // 
+            // label_chordi
+            // 
+            this.label_chordi.AutoSize = true;
+            this.label_chordi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_chordi.Location = new System.Drawing.Point(255, 280);
+            this.label_chordi.Name = "label_chordi";
+            this.label_chordi.Size = new System.Drawing.Size(29, 20);
+            this.label_chordi.TabIndex = 70;
+            this.label_chordi.Text = "N=";
+            // 
+            // label_chord
+            // 
+            this.label_chord.AutoSize = true;
+            this.label_chord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_chord.Location = new System.Drawing.Point(255, 260);
+            this.label_chord.Name = "label_chord";
+            this.label_chord.Size = new System.Drawing.Size(29, 20);
+            this.label_chord.TabIndex = 69;
+            this.label_chord.Text = "X=";
+            // 
+            // label_combi
+            // 
+            this.label_combi.AutoSize = true;
+            this.label_combi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_combi.Location = new System.Drawing.Point(255, 381);
+            this.label_combi.Name = "label_combi";
+            this.label_combi.Size = new System.Drawing.Size(29, 20);
+            this.label_combi.TabIndex = 72;
+            this.label_combi.Text = "N=";
+            // 
+            // label_comb
+            // 
+            this.label_comb.AutoSize = true;
+            this.label_comb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_comb.Location = new System.Drawing.Point(255, 361);
+            this.label_comb.Name = "label_comb";
+            this.label_comb.Size = new System.Drawing.Size(29, 20);
+            this.label_comb.TabIndex = 71;
+            this.label_comb.Text = "X=";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 20);
+            this.label1.TabIndex = 73;
+            this.label1.Text = "(x - 1)^2 - 1.5 * e^x = 0";
+            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(404, 240);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(23, 38);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 58;
-            this.label6.Text = "label6";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(404, 279);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
-            this.label7.TabIndex = 59;
-            this.label7.Text = "label7";
+            this.label6.Size = new System.Drawing.Size(90, 20);
+            this.label6.TabIndex = 74;
+            this.label6.Text = "Варіант 22";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_combi);
+            this.Controls.Add(this.label_comb);
+            this.Controls.Add(this.label_chordi);
+            this.Controls.Add(this.label_chord);
+            this.Controls.Add(this.label_tangenti);
+            this.Controls.Add(this.label_tangent);
+            this.Controls.Add(this.label_halfi);
+            this.Controls.Add(this.label_half);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox_b);
             this.Controls.Add(this.textBox_a);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.textBox_error);
             this.Controls.Add(this.button_clear);
@@ -197,6 +332,7 @@ namespace Lab_3_NM
             this.Controls.Add(this.button_exit);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,15 +345,26 @@ namespace Lab_3_NM
         private System.Windows.Forms.Button button_exit;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.TextBox textBox_error;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox_a;
         private System.Windows.Forms.TextBox textBox_b;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label_half;
+        private System.Windows.Forms.Label label_halfi;
+        private System.Windows.Forms.Label label_tangenti;
+        private System.Windows.Forms.Label label_tangent;
+        private System.Windows.Forms.Label label_chordi;
+        private System.Windows.Forms.Label label_chord;
+        private System.Windows.Forms.Label label_combi;
+        private System.Windows.Forms.Label label_comb;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
     }
 }
 
--- Lab_3_NM/Form1.cs
+++ Lab_3_NM/Form1.cs
@@ -24,11 +24,23 @@ namespace Lab_3_NM
 
         private void button_clear_Click(object sender, EventArgs e)
         {
+            label_halfi.Text = "N= ";
+            label_tangenti.Text = "N= ";
+            label_chordi.Text = "N= ";
+            label_combi.Text = "N= ";
+            label_half.Text = "X= ";
+            label_tangent.Text = "X= ";
+            label_chord.Text = "X= ";
+            label_comb.Text = "X= ";
+            textBox_error.Clear();
+            textBox_a.Clear();
+            textBox_b.Clear();
+
 
         }
         private void button_calculate_Click(object sender, EventArgs e)
         {
-            double x_s, a_s, a, a_h, b, b_h, b_s, a_ss, b_ss, error;
+            double a, b, error;
 
             double[] x = new double[4];
 
@@ -36,92 +48,181 @@ namespace Lab_3_NM
             a = Convert.ToDouble(textBox_a.Text);
             b = Convert.ToDouble(textBox_b.Text);
 
-            a_h = a;
-            b_h = b;
-            
+            x[0] = half_division_method(a, b, error);
+            label_half.Text = "X= " +  Convert.ToString(x[0]);
+
+            x[1] = tangent_method(a, b, error);
+            label_tangent.Text = "X= " + Convert.ToString(x[1]);
+
+            x[2] = chord_method(a, b, error);
+            label_chord.Text = "X= " + Convert.ToString(x[2]);
+
+            x[3] = combined_method(a, b, error);
+            label_comb.Text = "X= " + Convert.ToString(x[3]);
+
+
+        }
+
+        public double chord_method_func(double a_h, double b_h)
+        {
+            double x;
+            x = a_h - ((2 * Math.Pow(a_h, 2) * b_h - 2 * Math.Pow(a_h, 3) - 4 * a_h * b_h + 4 * Math.Pow(a_h, 2) + 2 * b_h - 2 * a_h - 3 * b_h * Math.Exp(a_h) + 3 * a_h * Math.Exp(a_h)) /
+                            (2 * Math.Pow(b_h, 2) - 4 * b_h - 3 * Math.Exp(b_h) - 2 * Math.Pow(a_h, 2) + 4 * a_h + 3 * Math.Exp(a_h)));
+            return x;
+        }
+
+        public double first_function(double a_h)
+        {
+            double x;
+
+            x = Math.Pow(a_h - 1, 2) - (1.5 * Math.Exp(a_h));
+
+            return x;
+        }
+
+        public double second_derivative(double a_h)
+        {
+            double x;
+
+            x = 2 - (3 * Math.Exp(a_h) / 2);
+
+            return x;
+        }
+
+        public double tangent_method_func(double b_h)
+        {
+            double x;
+            x = (2 * Math.Pow(b_h, 2) - 3 * b_h * Math.Exp(b_h) - 2 + 3 * Math.Exp(b_h)) / (4 * b_h - 4 - 3 * Math.Exp(b_h));
+            return x;
+        }
+
+        public double half_division_method(double a_h, double b_h, double error)
+        {
+            double x = 0, x_s, a_s;
+
             for (int i = 0; i < 1000; i++)
             {
-                x[0] = ((a_h + b_h) / 2);
+                x = ((a_h + b_h) / 2);
 
-                a_s = Math.Pow(a_h - 1, 2) - (1.5 * Math.Exp(a_h));
+                a_s = first_function(a_h);
 
-                x_s = Math.Pow(x[0] - 1, 2) - (1.5 * Math.Exp(x[0]));
+                x_s = first_function(x);
 
                 if (Math.Sign(x_s) == Math.Sign(a_s))
                 {
-                    a_h = x[0];
+                    a_h = x;
                 }
                 if (Math.Sign(x_s) != Math.Sign(a_s))
                 {
-                    b_h = x[0];
+                    b_h = x;
                 }
                 if (Math.Abs((b_h - a_h) / Math.Pow(2, i + 1)) <= error)
                 {
-                    break;
+                    label_halfi.Text = "N= " + Convert.ToString(i);
+                    return x;
                 }
             }
-            label1.Text = Convert.ToString(x[0]);
 
-            a_h = a;
-            b_h = b;
+            return x;
+        }
+
+        public double tangent_method(double a_h, double b_h, double error)
+        {
+            double x = 0, x_s, a_s, b_s, b_ss, a_ss;
 
             for (int i = 0; i < 1000; i++)
             {
-                x_s = x[1];
-                x[1] = (2 * Math.Pow(x[1], 2) - 3 * x[1] * Math.Exp(x[1]) - 2 + 3 * Math.Exp(x[1])) / (4 * x[1] - 4 - 3 * Math.Exp(x[1]));
-                a_s = Math.Pow(a_h - 1, 2) - (1.5 * Math.Exp(a_h));
-                b_s = Math.Pow(b_h - 1, 2) - (1.5 * Math.Exp(b_h));
+                x_s = x;
+                x = tangent_method_func(x);
 
-                a_ss = 2 - (3 * Math.Exp(a_h) / 2);
-                b_ss = 2 - (3 * Math.Exp(b_h) / 2);
-                
-                if (Math.Abs(x[1] - x_s) <= error)
+                a_s = first_function(a_h);
+                b_s = first_function(b_h);
+
+                a_ss = second_derivative(a_h);
+                b_ss = second_derivative(b_h);
+
+                if (Math.Abs(x - x_s) <= error)
                 {
-                    break;
+                    label_tangenti.Text = "N= " + Convert.ToString(i);
+                    return x;
                 }
+                if (a_s * a_ss > 0)
+                {
+                    a_h = x;
+                }
+                if (b_s * b_ss > 0)
+                {
+                    b_h = x;
+                }
+            }
+            return x;
+        }
+
+        public double chord_method(double a_h, double b_h, double error)
+        {
+            double x = 0, x_s, a_s, b_s, b_ss, a_ss, c;
+            c = a_h;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                x_s = x;
+
+                a_s = first_function(a_h);
+                b_s = first_function(b_h);
+
+                a_ss = second_derivative(a_h);
+                b_ss = second_derivative(b_h);
+
+                x = chord_method_func(x, c);
+
 
                 if (a_s * a_ss > 0)
                 {
-                    a_h = x[1];
+                    b_h = x;
+                    a_h = c;
                 }
 
                 if (b_s * b_ss > 0)
                 {
-                    b_h = x[1];
+                    a_h = x;
+                    b_h = c;
                 }
-                
+
+                if (Math.Abs(x - x_s) <= error)
+                {
+                    label_chordi.Text = "N=" + Convert.ToString(i);
+                    return x;
+                }
             }
+            return x;
+        }
 
-            label6.Text = Convert.ToString(x[1]);
-
-            a_h = a;
-            b_h = b;
+        public double combined_method(double a_h, double b_h, double error)
+        {
+            double x = 0;
 
             for (int i = 0; i < 1000; i++)
             {
-              
-                b_h = (2 * Math.Pow(b_h, 2) - 3 * b_h * Math.Exp(b_h) - 2 + 3 * Math.Exp(b_h)) / (4 * b_h - 4 - 3 * Math.Exp(b_h));
 
-                a_h = (2 * a_h * b_h - 2 + 3 * Math.Exp(x[2])) / (2 * b_h - 4 + 2 * a_h);
+                b_h = tangent_method_func(b_h);
 
-                
+                a_h = chord_method_func(a_h, b_h);
+
 
                 if (Math.Abs(b_h - a_h) <= error)
                 {
-                    break;
-                }  
+                    label_combi.Text = "N= " + Convert.ToString(i);
+                    return x;
+                }
 
-                x[2] = a_h + b_h / 2;
+                x = a_h + b_h / 2;
             }
-
-            label7.Text = Convert.ToString(x[2]);
-
+            return x;
         }
 
-        public double half_dec()
+        private void Form1_Load(object sender, EventArgs e)
         {
-            
-            return 0;
+
         }
     }
 }
